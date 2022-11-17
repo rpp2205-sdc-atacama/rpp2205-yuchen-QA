fde1984189033f9643ac5edc55f96f1d205c5278
Rain <pyc0422@gmail.com>
2022-11-17 00:02:37 -0600

    import whole data by using copy command

--- db/db.js
+++ db/db.js
@@ -13,35 +13,57 @@ pool
   .query(`CREATE TABLE IF NOT EXISTS questions (
     question_id SERIAL NOT NULL PRIMARY KEY,
     product_id INT NOT NULL,
-    asker_id INT NOT NULL,
-    question_body TEXT,
-    question_date timestamp with time zone NOT NULL DEFAULT current_timestamp,
+    asker_id INT,
+    question_body TEXT NOT NULL,
+    asker_name VARCHAR(50) NOT NULL,
+    asker_email VARCHAR(100) NOT NULL,
+    question_date VARCHAR(100) NOT NULL,
     question_helpfulness INT,
     reported boolean
   )`)
   .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS questions_seq START 1 INCREMENT 1 OWNED BY questions.question_id`))
-  .then(() => pool .query(`CREATE TABLE IF NOT EXISTS users (
+  .then(() => pool.query(`COPY questions(question_id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness)
+    FROM '/Users/rain/hackreactor/dataFiles/questions.csv' DELIMITER ',' CSV HEADER`))
+  .then((res) => {
+     console.log('copy lineL:', res);
+     return pool.query(`CREATE TABLE IF NOT EXISTS users (
       user_id SERIAL PRIMARY KEY,
       user_name VARCHAR(50),
       email VARCHAR(100)
-    )`))
-    .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS users_seq START 1 INCREMENT 1 OWNED BY users.user_id`))
-    .then(() =>  pool.query(`CREATE TABLE IF NOT EXISTS answers (
-      answer_id INT NOT NULL,
-      question_id INT NOT NULL,
-      answerer_id INT NOT NULL,
-      body TEXT,
-      date timestamp with time zone NOT NULL DEFAULT current_timestamp,
-      helpfulness INT,
-      reported boolean
-    )`))
-  .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS answers_seq START 1 INCREMENT 1 OWNED BY answers.answer_id`))
-  .then(() => pool.query(`CREATE TABLE IF NOT EXISTS photos(
-      photo_id INT NOT NULL,
-      url TEXT,
-      answer_id INT NOT NULL
-    )`))
-  .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS photos_seq START 1 INCREMENT 1 OWNED BY photos.photo_id`))
+    )`)
+  })
+  .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS users_seq START 1 INCREMENT 1 OWNED BY users.user_id`))
+  .then(() => pool.query(`INSERT INTO users(user_name, email) SELECT DISTINCT asker_name,asker_email FROM questions`))
+  .then((res) => {
+    console.log('user: ', res);
+    return pool.query(`UPDATE questions SET asker_id=users.user_id FROM users WHERE questions.asker_name=users.user_name AND questions.asker_email=users.email`)
+  })
+  .then((res) => {
+    console.log('update: ', res);
+    return pool.query('ALTER TABLE questions DROP COLUMN asker_name, DROP COLUMN asker_email')
+  })
+  .then(res => {
+    console.log('alter done!', res);
+  })
+  // .then(() =>  pool.query(`CREATE TABLE IF NOT EXISTS answers (
+  //     answer_id INT NOT NULL,
+  //     question_id INT NOT NULL,
+  //     answerer_id INT,
+  //     answerer_name VARCHAR(50) NOT NULL,
+  //     answerer_email VARCHAR(100) NOT NULL,
+  //     body TEXT NOT NULL,
+  //     date VARCHAR(100) NOT NULL,
+  //     helpfulness INT,
+  //     reported boolean
+  //   )`))
+  // .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS answers_seq START 1 INCREMENT 1 OWNED BY answers.answer_id`))
+  // .then(() => pool.query(`COPY answers(answer_id, question_id, body, date, answerer_name, answerer_)`))
+  // .then(() => pool.query(`CREATE TABLE IF NOT EXISTS photos(
+  //     photo_id INT NOT NULL,
+  //     url TEXT,
+  //     answer_id INT NOT NULL
+  //   )`))
+  // .then(() => pool.query(`CREATE SEQUENCE IF NOT EXISTS photos_seq START 1 INCREMENT 1 OWNED BY photos.photo_id`))
   .catch(err => console.error(err.stack));
 
 
